d58909cf8ae0d71e3bdc090bd4f5857d20bc2958
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2021-01-17 11:17:48 -0500

    completed read/write of CSV

--- TableConverter.java
+++ TableConverter.java
@@ -1,4 +1,8 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import javax.swing.JFileChooser;
 
 class TableConverter {
@@ -17,9 +21,8 @@ class TableConverter {
 
         System.out.println("The file " + file.getName() + " has been selected.");
 
-        // read the CSV file
-
-        // convert CSV contents into an HTML Table
+        // read the CSV file and convert to HTML Table
+        createTable(file);
 
     }
 
@@ -30,5 +33,44 @@ class TableConverter {
                 return jfc.getSelectedFile();
         }
         return jfc.getSelectedFile();
+    }
+
+    static void createTable(File f) {
+        try {
+            FileReader fr = new FileReader(f);
+            BufferedReader br = new BufferedReader(fr);
+            String table = "";
+            FileWriter writer = new FileWriter("table.html");
+
+            String line = "";
+            String [] cells;
+            while ((line = br.readLine()) != null) {
+                cells = line.split(",");
+
+                if (table == "") {
+                    table = "<!DOCTYPE html>\n<html>\n<table>\n<tr>\n";
+                    for (String cell : cells) {
+                        table += "<th>" + cell + "</th>\n";
+                    }
+                    table += "</tr>\n";
+                }
+                else {
+                    table += "<tr>\n"; // open the row
+                    for (String cell : cells) {
+                        table += "<td>" + cell + "</td>\n";
+                    }
+                    table += "</tr>\n"; // close the row
+                }
+            }
+            table += "</table>\n</html>\n";
+            System.out.println(table);
+            writer.write(table);
+            writer.close();
+
+            br.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
